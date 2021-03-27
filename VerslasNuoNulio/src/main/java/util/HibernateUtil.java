053009770d7cc96bf811571d122e2930e9667206
Modestas <mjovaisa@gmail.com>
2021-03-27 12:32:45 +0200

    Naujesnis

--- VerslasNuoNulio/src/main/java/util/HibernateUtil.java
+++ VerslasNuoNulio/src/main/java/util/HibernateUtil.java
@@ -21,7 +21,7 @@ public class HibernateUtil {
                 settings.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
                 settings.put(Environment.URL, "jdbc:mysql://localhost:3306/verslasnuonulio");
                 settings.put(Environment.USER, "root");
-                settings.put(Environment.PASS, "simonas1");
+                settings.put(Environment.PASS, "Modsas1994");
                 settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
                 settings.put(Environment.SHOW_SQL, "true");
                 settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
