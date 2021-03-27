f3d4e48845572dacb5a3c029f4a9a229cef944b7
Simas <gyd.stream@gmail.com>
2021-03-27 12:19:05 +0200

    Sukurtas hibernate

---
+++ VerslasNuoNulio/src/main/java/Entity/Sandelys.java
@@ -0,0 +1,80 @@
+package Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(schema = "verslasNuoNulio", name = "sandelys")
+public class Sandelys {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String pavadinimas;
+
+    @Column
+    private String kategorija;
+
+    @Column
+    private String spalva;
+
+    @Column
+    private int kiekis;
+
+    @Column
+    private double kaina;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPavadinimas() {
+        return pavadinimas;
+    }
+
+    public void setPavadinimas(String pavadinimas) {
+        this.pavadinimas = pavadinimas;
+    }
+
+    public String getKategorija() {
+        return kategorija;
+    }
+
+    public void setKategorija(String kategorija) {
+        this.kategorija = kategorija;
+    }
+
+    public String getSpalva() {
+        return spalva;
+    }
+
+    public void setSpalva(String spalva) {
+        this.spalva = spalva;
+    }
+
+    public int getKiekis() {
+        return kiekis;
+    }
+
+    public void setKiekis(int kiekis) {
+        this.kiekis = kiekis;
+    }
+
+    public double getKaina() {
+        return kaina;
+    }
+
+    public void setKaina(double kaina) {
+        this.kaina = kaina;
+    }
+}
--- VerslasNuoNulio/src/main/java/Parduotuve/Parduotuve.java
+++
@@ -1,4 +0,0 @@
-package Parduotuve;
-
-public class Parduotuve {
-}
