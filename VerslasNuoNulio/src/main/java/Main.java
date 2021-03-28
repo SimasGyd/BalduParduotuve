9dfec3972b88aa13b8e72a2bea8f2e489c1bc4bc
Modestas <mjovaisa@gmail.com>
2021-03-28 12:00:30 +0300

    Dao's create

--- VerslasNuoNulio/src/main/java/Main.java
+++ VerslasNuoNulio/src/main/java/Main.java
@@ -1,5 +1,4 @@
-import Entity.Sandelys;
-import Entity.SandelysDao;
+import Entity.*;
 
 import java.util.List;
 
@@ -8,12 +7,28 @@ public class Main {
         Sandelys sandelys = new Sandelys();
         SandelysDao sandelysDao = new SandelysDao();
 
-
         List<Sandelys> remainingPrekes = sandelysDao.getAllPrekes();
         System.out.println("List of all remaining Prekes in sandelys :");
         for (Sandelys sand : remainingPrekes) {
             System.out.println(sand);
         }
+        Pardavimai pardavimai = new Pardavimai();
+        PardavimaiDao pardavimaiDao = new PardavimaiDao();
+
+        List<Pardavimai> parduotosPrekes = PardavimaiDao.getParduotosPrekes();
+        System.out.println("List of all sold Prekes :");
+        for (Pardavimai pard : parduotosPrekes) {
+            System.out.println(pard);
+        }
+        Parduotuve parduotuve = new Parduotuve();
+        ParduotuveDao parduotuveDao = new ParduotuveDao();
+
+        List<Parduotuve> prekiuLikutis = ParduotuveDao.getAllPrekes();
+        System.out.println("List of all remaining Prekes in parduotuve :");
+        for (Parduotuve parda : prekiuLikutis) {
+            System.out.println(parda);
+        }
     }
 }
+
 
