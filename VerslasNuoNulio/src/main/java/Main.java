3334bd2d4abd13efd29b236cd2981f31e9231dd6
Simas <gyd.stream@gmail.com>
2021-03-30 12:50:23 +0300

    pritaikyti trys metodai likuciams gauti

--- VerslasNuoNulio/src/main/java/Main.java
+++ VerslasNuoNulio/src/main/java/Main.java
@@ -1,35 +1,9 @@
-import Entity.Sandelys;
-import Entity.SandelysDao;
 import Entity.*;
 
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Sandelys sandelys = new Sandelys();
-        SandelysDao sandelysDao = new SandelysDao();
-
-        List<Sandelys> remainingPrekes = sandelysDao.getAllPrekes();
-        System.out.println("List of all remaining Prekes in sandelys :");
-        for (Sandelys sand : remainingPrekes) {
-            System.out.println(sand);
-        }
-        Pardavimai pardavimai = new Pardavimai();
-        PardavimaiDao pardavimaiDao = new PardavimaiDao();
-
-        List<Pardavimai> parduotosPrekes = PardavimaiDao.getParduotosPrekes();
-        System.out.println("List of all sold Prekes :");
-        for (Pardavimai pard : parduotosPrekes) {
-            System.out.println(pard);
-        }
-        Parduotuve parduotuve = new Parduotuve();
-        ParduotuveDao parduotuveDao = new ParduotuveDao();
-
-        List<Parduotuve> prekiuLikutis = ParduotuveDao.getAllPrekes();
-        System.out.println("List of all remaining Prekes in parduotuve :");
-        for (Parduotuve parda : prekiuLikutis) {
-            System.out.println(parda);
-        }
         new Project();
     }
 }
--- VerslasNuoNulio/src/main/java/Services/DarbuotojoServises.java
+++ VerslasNuoNulio/src/main/java/Services/DarbuotojoServises.java
@@ -1,8 +1,52 @@
 package Services;
 
+import Entity.Pardavimai;
+import Entity.Parduotuve;
+import Entity.Sandelys;
+import Util.HibernateUtil;
+import org.hibernate.Session;
 import repository.sandelys.SandelysRepository;
+
+import java.util.List;
 
 public class DarbuotojoServises {
     public DarbuotojoServises(SandelysRepository sandelysRepository) {
+    }
+    public static List<Parduotuve> prekiuLikutisParduotuveje() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+
+        @SuppressWarnings("unchecked")
+        List<Parduotuve> prekesByName = (List<Parduotuve>) session.createQuery(
+                "FROM Preke s left join Entity.Parduotuve a on s.id = a.prekesid ORDER BY s.id ASC").list();
+
+        session.getTransaction().commit();
+        session.close();
+        return prekesByName;
+    }
+
+    public List<Sandelys> prekiuLikutisSandelyje() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+
+        @SuppressWarnings("unchecked")
+        List<Sandelys> prekesByName = (List<Sandelys>) session.createQuery(
+                "FROM Preke s left join Entity.Sandelys a on s.id = a.prekesid ORDER BY s.id ASC").list();
+
+        session.getTransaction().commit();
+        session.close();
+        return prekesByName;
+    }
+    public static List<Pardavimai> pardavimaiList() {
+        Session session = HibernateUtil.getSessionFactory().openSession();
+        session.beginTransaction();
+
+        @SuppressWarnings("unchecked")
+        List<Pardavimai> parduotosPrekes = (List<Pardavimai>) session.createQuery(
+                "FROM Preke s left join Entity.Pardavimai a on s.id = a.prekesid ORDER BY s.id ASC").list();
+
+        session.getTransaction().commit();
+        session.close();
+        return parduotosPrekes;
     }
 }
